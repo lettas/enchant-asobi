6ba9265d82c728def15be4d5a38b338a790167a5
Toru Matsuoka <lettas0726@gmail.com>
2013-09-02 23:51:36 +0900

    Add wall

--- jumpbare/js/main.js
+++ jumpbare/js/main.js
@@ -6,31 +6,49 @@ var physicsWorld;
 window.onload = function() {
   game = new Core(320, 480);
   game.fps = 30;
-  game.preload(['img/chara1.png']);
-  game.rootScene.backgroundColor = '#000000';
+  game.preload(['img/chara1.png', 'img/map0.png']);
+  game.rootScene.backgroundColor = '#99AAFF';
   game.onload = function() {
     physicsWorld = new PhysicsWorld(0.0, 9.8);
 
     var scene = game.rootScene;
-    scene.onenterframe = function(e) {
-      physicsWorld.step(game.fps);
-    };
 
     var player = new Player();
-    player.position = {x:160, y:240};
+    player.position = {x: 160, y: 240};
     scene.addChild(player);
+
+    for (var i = 0; i < 320 / 16; i++) {
+      var ground = new Map(7);
+      ground.position = {x: i * 16 + 8, y: 480 - 16 + 8};
+      scene.addChild(ground);
+    }
+
+    for (var i = 0; i < (480 / 16) - 1; i++) {
+      var wall = new Map(3);
+      wall.position = {x: 8, y: i * 16 + 8};
+      scene.addChild(wall);
+
+      var wall = new Map(3);
+      wall.position = {x: 320 - 8, y: i * 16 + 8};
+      scene.addChild(wall);
+    }
 
     scene.ontouchstart = function(e) {
       var vx = (e.x > 160) ? 25 : -25;
       player.jump(vx);
+      player.setAwake(true);
     }
+
+    scene.onenterframe = function(e) {
+      physicsWorld.step(game.fps);
+    };
   };
   game.start();
 };
 
 var Player = Class.create(PhyBoxSprite, {
     initialize: function () {
-      PhyBoxSprite.call(this, 32, 32, enchant.box2d.DYNAMIC_SPRITE, 1.0, 0.9, 0.3, true);
+      PhyBoxSprite.call(this, 32, 32, enchant.box2d.DYNAMIC_SPRITE, 0.0, 0.5, 0, true);
       this.image = game.assets['img/chara1.png'];
       this.frame = 0;
     },
@@ -44,5 +62,13 @@ var Player = Class.create(PhyBoxSprite, {
       this.vx = vx;
       this.vy = -200;
     }
+});
+
+var Map = Class.create(PhyBoxSprite, {
+    initialize: function (n) {
+      PhyBoxSprite.call(this, 16, 16, enchant.box2d.STATIC_SPRITE, 0.0, 0.5, 0, true);
+      this.image = game.assets['img/map0.png'];
+      this.frame = n;
+    },
 });
 
