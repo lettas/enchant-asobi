eb4488d56deb09da59b4e1c2a75513cfc9613f04
Toru Matsuoka <lettas0726@gmail.com>
2013-09-10 08:40:24 +0900

    Use constants

--- jumpbare/js/main.js
+++ jumpbare/js/main.js
@@ -1,10 +1,20 @@
 enchant();
+var CANVAS_WIDTH = 320;
+var CANVAS_HEIGHT = 320;
+var MAPCHIP_GROUND = 7;
+var MAPCHIP_WALL = 3;
+var PLAYER_SPRITE_WIDTH = 32;
+var PLAYER_SPRITE_HEIGHT = 32;
+var MAPCHIP_SPRITE_WIDTH = 16;
+var MAPCHIP_SPRITE_HEIGHT = 16;
+var EFFECT_SPRITE_WIDTH = 16;
+var EFFECT_SPRITE_HEIGHT = 16;
 
 var game;
 var physicsWorld;
 
 window.onload = function() {
-  game = new Core(640, 960);
+  game = new Core(CANVAS_WIDTH, CANVAS_HEIGHT);
   game.fps = 30;
   game.preload(['img/chara1.png', 'img/map0.png', 'img/effect0.png']);
   game.rootScene.backgroundColor = '#99AAFF';
@@ -14,22 +24,22 @@ window.onload = function() {
     var scene = game.rootScene;
 
     var player = new Player();
-    player.position = {x: 160, y: 240};
+    player.position = {x: game.width / 2, y: game.height / 2};
     scene.addChild(player);
 
-    for (var i = 0; i < 320 / 16; i++) {
-      var ground = new MapChip(7);
-      ground.position = {x: i * 16 + 8, y: 480 - 16 + 8};
+    for (var i = 0; i < game.width / MAPCHIP_SPRITE_WIDTH; i++) {
+      var ground = new MapChip(MAPCHIP_GROUND);
+      ground.position = {x: i * MAPCHIP_SPRITE_WIDTH + MAPCHIP_SPRITE_WIDTH / 2, y: game.height - MAPCHIP_SPRITE_HEIGHT + MAPCHIP_SPRITE_HEIGHT / 2};
       scene.addChild(ground);
     }
 
-    for (var i = 0; i < (480 / 16) - 1; i++) {
-      var wall = new MapChip(3);
-      wall.position = {x: 8, y: i * 16 + 8};
+    for (var i = 0; i < (game.height/ MAPCHIP_SPRITE_HEIGHT) - 1; i++) {
+      var wall = new MapChip(MAPCHIP_WALL);
+      wall.position = {x: MAPCHIP_SPRITE_WIDTH / 2, y: i * MAPCHIP_SPRITE_HEIGHT + MAPCHIP_SPRITE_HEIGHT / 2};
       scene.addChild(wall);
 
-      var wall = new MapChip(3);
-      wall.position = {x: 320 - 8, y: i * 16 + 8};
+      var wall = new MapChip(MAPCHIP_WALL);
+      wall.position = {x: game.width - MAPCHIP_SPRITE_WIDTH / 2, y: i * MAPCHIP_SPRITE_HEIGHT + MAPCHIP_SPRITE_HEIGHT / 2};
       scene.addChild(wall);
     }
 
@@ -39,7 +49,7 @@ window.onload = function() {
       player.setAwake(true);
 
       var effect = new Effect();
-      effect.x = player.x + 8;
+      effect.x = player.x + EFFECT_SPRITE_WIDTH;
       effect.y = player.y + player.height;
       scene.addChild(effect);
     }
@@ -53,7 +63,7 @@ window.onload = function() {
 
 var Player = Class.create(PhyBoxSprite, {
     initialize: function () {
-      PhyBoxSprite.call(this, 32, 32, enchant.box2d.DYNAMIC_SPRITE, 0.0, 0.5, 0, true);
+      PhyBoxSprite.call(this, PLAYER_SPRITE_WIDTH, PLAYER_SPRITE_HEIGHT, enchant.box2d.DYNAMIC_SPRITE, 0.0, 0.5, 0, true);
       this.image = game.assets['img/chara1.png'];
       this.frame = 0;
     },
@@ -71,7 +81,7 @@ var Player = Class.create(PhyBoxSprite, {
 
 var MapChip = Class.create(PhyBoxSprite, {
     initialize: function (n) {
-      PhyBoxSprite.call(this, 16, 16, enchant.box2d.STATIC_SPRITE, 0.0, 0.5, 0, true);
+      PhyBoxSprite.call(this, MAPCHIP_SPRITE_WIDTH, MAPCHIP_SPRITE_HEIGHT, enchant.box2d.STATIC_SPRITE, 0.0, 0.5, 0, true);
       this.image = game.assets['img/map0.png'];
       this.frame = n;
     },
@@ -79,7 +89,7 @@ var MapChip = Class.create(PhyBoxSprite, {
 
 var Effect = Class.create(Sprite, {
     initialize: function () {
-      Sprite.call(this, 16, 16);
+      Sprite.call(this, EFFECT_SPRITE_WIDTH, EFFECT_SPRITE_HEIGHT);
       this.image = game.assets['img/effect0.png'];
       this.frame = 0;
     },
